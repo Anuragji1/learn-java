bca6d0a9e7ce2ebbb174734ab8de3d1801f42672
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-02-13 18:01:23 +0530

    Create printing-text.java

---
+++ printing-text.java
@@ -0,0 +1,28 @@
+**Printing Text**
+
+Statement 2) Printing text
+In the last problem, we saw how to print a number (using System.out.println).
+
+Let's see how to output some text.
+
+There is only one difference with printing a number: all textual content should start and end with double quotes ("").
+
+For example, we output a number like this
+
+System.out.println(10);
+But to output a text, we do this
+
+System.out.println("I love Java");
+Task
+In this problem, we want to output "I love Java". For that, you have to edit the code present in editor. The editor has some __ (underscores). Your task is to remove underscores and write "I love Java", to solve the problem.
+
+We add underscores in editor so that you know which portion of code to edit.
+
+---------------------------------------------------------------------------------
+SOLUTION - class CodeChef
+{
+    public static void main (String[] args)
+    {
+        System.out.println("I love Java");
+    }
+}
