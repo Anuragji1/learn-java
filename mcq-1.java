bd5ca0119cd1940b86b1ac24f82debddfb382a7c
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-02-15 18:17:57 +0530

    Create mcq-1.java

---
+++ mcq-1.java
@@ -0,0 +1,16 @@
+**MCQ-1**
+
+Statement) Which line of code will output the string "Hi"?
+
+Sys.out.println("Hi");
+
+System.out.println("Hi");
+
+System.out.println(Hi);
+
+System.out.println("Hi")
+
+-----------------------------------------------------------------------------------
+  SOLUTION = Correct Answer:
+
+System.out.println("Hi");
