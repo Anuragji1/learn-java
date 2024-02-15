1703f25d4188585202620053e3253031d03547fb
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-02-15 18:19:31 +0530

    Create mc-2.java

---
+++ mc-2.java
@@ -0,0 +1,16 @@
+**MCQ-2**
+
+Statement) Which line of code will output the sum of 7 and 19?
+
+System.println(7 + 19)
+
+System.out.println("7 + 19");
+
+system.out.println(7 + 19);
+
+System.out.println(7 + 19);
+
+--------------------------------------------------------------------------------------
+  SOLUTION - Correct Answer:
+
+System.out.println(7 + 19);
